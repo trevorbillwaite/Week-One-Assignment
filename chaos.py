38d4db0572233b206cec673aff49f3e6fa703348
trevorbillwaite <trev.waite@myemail.indwes.edu>
2015-09-11 18:32:00 +0000

    asdf

--- chaos.py
+++ chaos.py
@@ -2,12 +2,14 @@ __author__ = 'daniel.neumann'
 
 # File: chaos.py
 # A simple program illustrating chaotic behavior.
-
+multiplier=2.0
+loopCounter=10
 def main():
     print("This program illustrates a chaotic function")
     x = eval(input("Enter a number between 0 and 1: "))
-    for i in range(10):
-        x = 3.9 * x * (1 - x)
+    for i in range(loopCounter):
+        x = (multiplier) * x * (1 - x)
         print(x)
 
 main()
+
